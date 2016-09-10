b385b8d3c4bd888c4e707c8de19f3e563edf2a49
[reza]™ <agung.rezartha@icehousecorp.com>
2016-09-10 17:44:16 +0700

    mobile adjustment

--- __templates/main_site/src/assets/scripts/scripts.js
+++ __templates/main_site/src/assets/scripts/scripts.js
@@ -1,2 +1,8 @@
 //= require ./vendor/bootstrap-sprockets.js
 //= require ./vendor/jasny-bootstrap.min.js
+
+jQuery(window).ready(function($){
+  $('.to-the-top').click(function(){
+    $('body, html').animate({scrollTop: 0});
+  })
+});
