bb29895c5b21679acb3be68f9d102e17988a2cd9
[reza]™ <agung.rezartha@icehousecorp.com>
2016-09-17 19:21:29 +0700

    add active state for menu items adjust menu style

--- __templates/main_site/src/assets/scripts/scripts.js
+++ __templates/main_site/src/assets/scripts/scripts.js
@@ -4,5 +4,6 @@
 jQuery(window).ready(function($){
   $('.to-the-top').click(function(){
     $('body, html').animate({scrollTop: 0});
-  })
+  });
+  $('.navbar a[href="/' + location.pathname.split("/")[1] + '"]').parent().addClass('active');
 });
